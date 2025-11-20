5a71c79cb4ceb1e7064aaf7381e37955ce7b7b87
manojsonikgn-mks <manojsonikgn@jitechno.com>
2025-11-20 07:25:14 +0530

    commit multiple files

---
+++ calendar module.py
@@ -0,0 +1,25 @@
+import calendar as c
+
+#print(c.isleap(2000))
+#print(c.isleap(2003))
+
+
+#help("calendar")
+
+#print(c.leapdays(2000,2020))
+
+#print(c.calendar(2025))
+
+#print(c.calendar(2025,w=2,l=2,c=2))#width,length,space b/w months
+
+#print(c.calendar(2025,m=2))#m=number of columns
+
+#print(c.monthcalendar(2025,10))#shows october month in matrix format
+
+#print(c.weekday(2025,10,28))#shows day on basis of day(yyyy,mm,dd)
+
+'''a=c.weekday(2025,10,28)
+if a==1:
+    print("Tuesday")'''
+
+#print(c.monthrange(2025,10))#output (calendar.WEDNESDAY, 31)
